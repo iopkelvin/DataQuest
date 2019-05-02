26868ec8d127ebc2c6bfe3d73014429fa4129318
Kelvin Ortiz <kelvin@Kelvins-MacBook-Pro.local>
2019-05-02 10:28:33 -0700

    Data Cleaning and Analysis

---
+++ Command Line Basics/Code Work/Guided Project: Working With Data Downloads-220.sh
@@ -0,0 +1,15 @@
+## 3. Using Pandas to Find Patterns in the Data ##
+
+(python3) /home/dq/scripts$ ls
+
+## 4. Using Pandas to Explore Enrollment by Race ##
+
+bash: /home/dq/scripts$: No such file or directory
+
+## 5. Moving the Data Files to a Separate Folder ##
+
+(python3) /home/dq/scripts$ ls
+
+## 6. Next Steps ##
+
+/home/dq/scripts$ ls
---
+++ Command Line Basics/Guided Project_ Working With Data Downloads/exploration.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+	import pandas as pd
+	data = pd.read_csv("data/CRDC2013_14.csv", encoding="Latin-1")
+	print(data["JJ"].value_counts())
+	print(data["SCH_STATUS_MAGNET"].value_counts())
+	jj = pd.pivot_table(data, values=["TOT_ENR_M", "TOT_ENR_F"], index="JJ", aggfunc="sum")
+	magnet = pd.pivot_table(data, values=["TOT_ENR_M", "TOT_ENR_F"], index="SCH_STATUS_MAGNET", aggfunc="sum")	
+	print(jj)
+	print(magnet)
